0789e72806fef6fa4000475a0d59d6cb3e31dcd2
Robin Schleser <robinschleser0608@gmail.com>
2020-07-19 14:27:55 +0200

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,20 +32,10 @@ function revealCurtain(parent) {
 };
 
 function msieversion() {
-
   var ua = window.navigator.userAgent;
   var msie = ua.indexOf("MSIE ");
 
-  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
-  {
-      return true;
-  }
-  else
-  {
-      return false;
-  }
-
-  return false;
+  return (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))
 }
 
 function init () {
